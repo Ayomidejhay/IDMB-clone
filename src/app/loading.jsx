fe4485c64568b4a70cc1e89f9ff2e47062844280
Ayomidejhay <ayomiolaniyan@gmail.com>
2024-11-19 06:01:28 +0100

    MovieCard

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+// loading.io
+
+const loading = () => {
+  return (
+    <div className="flex justify-center h-screen items-center">
+        <img src="spinner.svg" alt="loading..." />
+    </div>
+  )
+}
+
+export default loading
